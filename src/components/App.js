2c31690b05f8964a6bbbf1c41cebb32006c1fbac
Sampat Kumar <mpskumar075@gmail.com>
2021-07-06 01:58:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,34 +2,27 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			showPara: false;
-		};
-	}
-
+    constructor(props) {
+		super(props);
+	    this.state={msg : ""}
+	    this.show=this.show.bind(this)
+	};
 	
-renderPara() {
-this.setState((curState) => {
-    return {showPara: !curState.showPara};
-});
-}
+	show() {
+	this.setState({msg : <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>})
+	}
 
     render() {
     	return(
     		<div id="main">
-				
-       <button id="click" onClick={this.renderPara.bind(this)}>Click!!</button>
-       {
-           this.state.showPara?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null
-       }
-        </div>
-    		
+				{ /* Do not remove this main div!! */ }
+		
+		<button id="click" onClick={this.show}>click</button>
+		{this.state.msg}
+    		</div>
     	);
     }
 }
 
 
 export default App;
-
